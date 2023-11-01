b9aef966823b87146b3511971215d6d183119474
Omar Abdullah <omar.amaghawry@gmail.com>
2023-11-02 00:24:35 +0200

    merge

--- src/main/java/com/zfinance/controller/systemOperation/SystemOperationController.java
+++ src/main/java/com/zfinance/controller/systemOperation/SystemOperationController.java
@@ -1,11 +1,5 @@
 package com.zfinance.controller.systemOperation;
 
-
-import com.zfinance.dto.request.systemOperation.SystemOperationReqDTO;
-import com.zfinance.dto.response.systemOperation.SystemOperationResDTO;
-import com.zfinance.mapper.SystemOperationMapper;
-import com.zfinance.orm.systemOperation.SystemOperation;
-import com.zfinance.services.systemOperation.SystemOperationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,36 +11,40 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.zfinance.dto.request.systemOperation.SystemOperationReqDTO;
+import com.zfinance.dto.response.systemOperation.SystemOperationResDTO;
+import com.zfinance.mapper.SystemOperationMapper;
+import com.zfinance.orm.systemOperation.SystemOperation;
+import com.zfinance.services.systemOperation.SystemOperationService;
 
 @RestController
 @RequestMapping("/systemOperation")
 @CrossOrigin("*")
 public class SystemOperationController {
 
-    @Autowired
-    SystemOperationService systemOperationService;
+	@Autowired
+	SystemOperationService systemOperationService;
 
-    @GetMapping
-    public SystemOperationResDTO getAllOperation(){
-        SystemOperationResDTO systemOperationResDTO= new SystemOperationResDTO();
-        systemOperationResDTO.setRecords(systemOperationService.getSystemOperations());
-        return systemOperationResDTO;
-    }
+	@GetMapping
+	public SystemOperationResDTO getAllOperation() {
+		SystemOperationResDTO systemOperationResDTO = new SystemOperationResDTO();
+		systemOperationResDTO.setRecords(systemOperationService.getSystemOperations());
+		return systemOperationResDTO;
+	}
 
-    @PostMapping
-    public SystemOperation CreateSystemOperation(@RequestBody SystemOperationReqDTO reqDto){
-        return systemOperationService.createSystemOperation(
-                SystemOperationMapper.INSTANCE.mapSystemOperationReqDto(reqDto));
-    }
+	@PostMapping
+	public SystemOperation CreateSystemOperation(@RequestBody SystemOperationReqDTO reqDto) {
+		return systemOperationService.createSystemOperation(SystemOperationMapper.INSTANCE.mapSystemOperationReqDto(
+				reqDto));
+	}
 
-    @PatchMapping("/{id}")
-    public SystemOperation updateSystemOperation(@PathVariable String id, @RequestBody SystemOperation req){
-        return systemOperationService.updateSystemOperation(id,req);
-    }
+	@PatchMapping("/{id}")
+	public SystemOperation updateSystemOperation(@PathVariable String id, @RequestBody SystemOperation req) {
+		return systemOperationService.updateSystemOperation(id, req);
+	}
 
-    @DeleteMapping("/{id}")
-    public SystemOperation deleteSystemOperation(@PathVariable String id){
-        return systemOperationService.deleteSystemOperation(id);
-    }
+	@DeleteMapping("/{id}")
+	public SystemOperation deleteSystemOperation(@PathVariable String id) {
+		return systemOperationService.deleteSystemOperation(id);
+	}
 }
--- src/main/java/com/zfinance/repositories/systemOperation/SystemOperationRepository.java
+++ src/main/java/com/zfinance/repositories/systemOperation/SystemOperationRepository.java
@@ -1,12 +1,9 @@
 package com.zfinance.repositories.systemOperation;
 
-import com.zfinance.orm.coin.Wallet;
-import com.zfinance.orm.systemOperation.SystemOperation;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.zfinance.orm.systemOperation.SystemOperation;
 
 @Repository
 public interface SystemOperationRepository extends MongoRepository<SystemOperation, String> {
